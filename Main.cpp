73148ad1856d69ffeff0104ca91d6553c2ffa13e
Darkil6 <darkilchelter@gmail.com>
2023-10-31 18:43:49 +0100

    debil

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <direct.h>
+int main()
+{
+	srand(static_cast<unsigned>(time(0)));
+	
+	const char* foldername = "MyFolderBitch";
+	int result = _mkdir(foldername);
+	int result2 = _mkdir("name");
+	if (result != 0)
+	{
+		std::cout << "wrong" << std::endl;
+	}
