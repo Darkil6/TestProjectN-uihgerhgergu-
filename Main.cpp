f74742946b5ad65a3495b4bd9bb328fe7dd77ae9
tsdd19 <7tenu.tenu7@gmail.com>
2023-11-02 01:17:19 +0200

    Modified Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,26 +1,20 @@
-//This My File C++
-//
-//
-///
-//
-//
-//
-///
-//
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <cstring>
-#include <direct.h>
+#include <sys/types.h>
+
 int main()
 {
 	srand(static_cast<unsigned>(time(0)));
 	
 	const char* foldername = "MyFolderBitch";
-	int result = _mkdir(foldername);
-	int result2 = _mkdir("name");
+	int result = P_tmpdir(foldername);
+	int result2 = P_tmpdir("name");
 	if (result != 0)
 	{
 		std::cout << "wrong" << std::endl;
 	}
+    
+    return 0;
 }
